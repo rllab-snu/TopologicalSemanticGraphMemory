3282271adcefd0bb63ce3081d328b3593d33ddff
Nuri Kim <nuri.kim@rllab.snu.ac.kr>
2022-10-12 11:56:53 +0900

    init

--- trainer/rl/ppo/__init__.py
+++ trainer/rl/ppo/__init__.py
@@ -1,5 +1,4 @@
-from .policy import Net, PointNavBaselinePolicy, Policy
 from .ppo import PPO
 from .ppo_trainer_memory import PPOTrainer_Memory
 
-__all__ = ["PPO", "Policy", "Net", "PointNavBaselinePolicy", "PPOTrainer_Memory"]
+__all__ = [ "PPOTrainer_Memory"]
